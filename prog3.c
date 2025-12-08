e11ddc1150d3b467f44f8eafb967c2a80c3c79e8
sharadha <saisharadha.cs24@bmsce.ac.in>
2025-12-08 10:02:44 +0530

    added comment to the code

--- prog3.c
+++ prog3.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-#define MAX 5
+#define MAX 5  //size of the queue
 
 int queue[MAX];
 int front = -1, rear = -1;
 
-void insert() {
+void insert() {    //insert elements into the queue
     int item;
     printf("Enter the element to insert: ");
     scanf("%d", &item);
